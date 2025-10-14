c4fd188d1aab1c768a228e12d57835a8a0af1a09
Tjasa <tjasazzeleznik@gmail.com>
2025-10-14 20:53:39 +0200

    Main title color to red

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
   return (
     <>
       <div className="w-screen h-screen bg-main flex flex-col justify-center items-center">
-        <h1 className="text-4xl text-indigo-50 font-bold">Calculator</h1>
+        <h1 className="text-4xl text-red-600 font-bold">Calculator</h1>
 
         <div className="bg-indigo-50 h-150 w-110 p-8 rounded-3xl">
           <form onSubmit={(e) => {
